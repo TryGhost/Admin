d37cb308703cacc032abe24f5bc12fbeab420131
Austin Burdine <acburdine@gmail.com>
2016-02-01 14:51:46 -0600

    first pass at wiring tour component [ci skip]

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -22,10 +22,6 @@ export default Controller.extend({
     actions: {
         toggleDeletePostModal() {
             this.toggleProperty('showDeletePostModal');
-        },
-
-        togglePopover() {
-            this.toggleProperty('showPopover');
         }
     }
 });
