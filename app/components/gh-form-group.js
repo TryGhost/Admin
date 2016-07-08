4954a1c0d3e165d35c2fc97984c4350c8db13bab
Austin Burdine <acburdine@gmail.com>
2016-07-07 21:43:38 -0600

    more deletion of things [ci skip]

--- app/components/gh-form-group.js
+++
@@ -1,5 +0,0 @@
-import ValidationStatusContainer from 'ghost-admin/components/gh-validation-status-container';
-
-export default ValidationStatusContainer.extend({
-    classNames: 'form-group'
-});
