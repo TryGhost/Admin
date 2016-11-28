22afd9d0429f043a2a86e563cae86ddeae9da66e
Austin Burdine <acburdine@gmail.com>
2016-11-28 16:30:01 -0600

    :racehorse: deps:ember-exam@0.6.0

--- testem.js
+++ testem.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 module.exports = {
     'framework': 'mocha',
+    'parallel': 2,
     'test_page': 'tests/index.html?hidepassed',
     'disable_watching': true,
     'launch_in_ci': [
