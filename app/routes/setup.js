TryGhost/Admin

commit 02f67a3d66788ce6e02bec60ddb4ba24431d0bf3

Signup flow token refresh for auto-login (#433)
refs https://github.com/TryGhost/Ghost/pull/7765
- if we have a `token` query param during setup, exchange it for a proper access token and authenticate the session

app/routes/setup.js
@@ -2,15 +2,17 @@ import Route from 'ember-route';
 import injectService from 'ember-service/inject';
 import Configuration from 'ember-simple-auth/configuration';
 import styleBody from 'ghost-admin/mixins/style-body';
+import {isBlank} from 'ember-utils';
 
 export default Route.extend(styleBody, {
     titleToken: 'Setup',
 
     classNames: ['ghost-setup'],
 
+    ajax: injectService(),
+    config: injectService(),
     ghostPaths: injectService(),
     session: injectService(),
-    ajax: injectService(),
 
     // use the beforeModel hook to check to see whether or not setup has been
     // previously completed.  If it has, stop the transition into the setup page.
@@ -20,6 +22,31 @@ export default Route.extend(styleBody, {
         if (this.get('session.isAuthenticated')) {
             this.transitionTo(Configuration.routeIfAlreadyAuthenticated);
             return;
+        }
+
+        // After ghost.org signup we get redirected to setup/three with a
+        // one-time, short expiry token in order to finish the signup flow.
+        // Grab the token here, exchange it for a full token set and allow
+        // setup to continue
+        let [, token] = window.location.search.match(/token=(.*)/);
+
+        if (!isBlank(token)) {
+            let tokenExchangeUrl = this.get('ghostPaths.url')
+                .api('authentication', 'setup', 'three');
+
+            // simulate a completed step 2 and skip other setup bits
+            this.controllerFor('setup.two').set('blogCreated', true);
+
+            return this.get('ajax')
+                .post(tokenExchangeUrl, {data: {
+                    token,
+                    client_id: this.get('config.clientId'),
+                    client_secret: this.get('config.clientSecret')
+                }}).then((data) => {
+                    return this.get('session').session.store.restore(data);
+                }).catch(() => {
+                    return this.transitionTo('signin');
+                });
         }
 
         let authUrl = this.get('ghostPaths.url').api('authentication', 'setup');

commit e74ad152f9f1692be143d122185eef4229918156

🐛 fix non-token signup
no issue
- fix issue introduced in https://github.com/TryGhost/Ghost-Admin/pull/433 that prevented /setup from working if no token was provided

app/routes/setup.js
@@ -28,7 +28,7 @@ export default Route.extend(styleBody, {
         // one-time, short expiry token in order to finish the signup flow.
         // Grab the token here, exchange it for a full token set and allow
         // setup to continue
-        let [, token] = window.location.search.match(/token=(.*)/);
+        let [, token] = window.location.search.match(/token=(.*)/) || [];
 
         if (!isBlank(token)) {
             let tokenExchangeUrl = this.get('ghostPaths.url')