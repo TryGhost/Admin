ec412abf211ad7fb9608d492e186b90cd8433f25
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-01-05 17:08:07 +0000

    fix tests on LTS
    no issue
    - `liquid-tether` which we use for modals leaves container elements behind when it's used within acceptance tests which then interfere with the integration tests because integration tests expect the first element in the testing container to be the rendered component

--- tests/helpers/destroy-app.js
+++ tests/helpers/destroy-app.js
@@ -1,5 +1,11 @@
 import run from 'ember-runloop';
+import $ from 'jquery';
 
 export default function destroyApp(application) {
+    // this is required because it gets injected during acceptance tests but
+    // not removed meaning that the integration tests grab this element rather
+    // than their rendered content
+    $('.liquid-target-container').remove();
+
     run(application, 'destroy');
 }
