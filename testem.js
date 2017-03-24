447bde5529208b101543a04cc4a719570970aa10
Ryan McCarvill <ryan@disordinary.com>
2017-03-25 01:36:12 +1300

    ✨  Selected card
    Refs: https://github.com/TryGhost/Ghost/issues/8191
    Refs: https://github.com/TryGhost/Ghost/issues/8194
    
    Changes the selection behaviour of mobiledoc-cards:
    If you navigate to a card with a keyboard or click on the new editor toolbar it "hard selects".
    If you click into the body of a card to edit it it "soft selects".
    
    When a card is "hard selected" you can navigate out of the card and to the previous or following blocks within the mobiledoc with the keyboard, you can delete the current card with the backspace or delete button, and you can create a new block following the card with the enter key.
    
    When a card is soft selected it is simply displayed as selected and allows the user to edit content within the card.
    
    New card toolbar:
    Allows a user to delete the card, save the card, and "hard select" a card.
    
    New title behaviour:
    Pressing the enter key within the title "splits" the title at the cursor point, if multiple characters are selected they are first deleted, and creates a new paragraph at the top of the document with the trailing characters after the split.
    
    gh-cm-editor updates:
    Adds an on-focus event to gh-cm-editor

--- testem.js
+++ testem.js
@@ -4,11 +4,9 @@ module.exports = {
     'test_page': 'tests/index.html?hidepassed',
     'disable_watching': true,
     'launch_in_ci': [
-        'Chrome',
-        'Firefox'
+        'Chrome'
     ],
     'launch_in_dev': [
-        'Chrome',
-        'Firefox'
+        'Chrome'
     ]
 };
