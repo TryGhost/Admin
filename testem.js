be294f136c6734e1f5b9a2b5853b93448bc6deb9
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-03-30 15:16:56 +0100

    re-enable Firefox in Travis tests

--- testem.js
+++ testem.js
@@ -4,9 +4,11 @@ module.exports = {
     'test_page': 'tests/index.html?hidepassed',
     'disable_watching': true,
     'launch_in_ci': [
-        'Chrome'
+        'Chrome',
+        'Firefox'
     ],
     'launch_in_dev': [
-        'Chrome'
+        'Chrome',
+        'Firefox'
     ]
 };
