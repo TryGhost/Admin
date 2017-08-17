a9ae452e870e2f6199364c11fa0a5990e56dc6f4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-08-17 16:43:58 +0100

    fetch mailing lists via server

--- app/controllers/settings/apps/mailchimp.js
+++ app/controllers/settings/apps/mailchimp.js
@@ -1,19 +1,26 @@
 import Controller from 'ember-controller';
 import injectService from 'ember-service/inject';
 import {alias, empty} from 'ember-computed';
+import {readOnly} from '@ember/object/computed';
 import {task} from 'ember-concurrency';
 
 export default Controller.extend({
+    ajax: injectService(),
+    ghostPaths: injectService(),
     notifications: injectService(),
     settings: injectService(),
 
     model: alias('settings.mailchimp'),
     testRequestDisabled: empty('model.apiKey'),
-    apiUrl: 'https://us16.api.mailchimp.com/3.0',
 
-    isFetchingLists: false,
-    availableLists: [],
-    _scratchApiKey: null,
+    availableLists: null,
+
+    isFetchingLists: readOnly('_fetchMailingLists.isRunning'),
+
+    init() {
+        this._super(...arguments);
+        this.set('availableLists', []);
+    },
 
     _triggerValidations() {
         let isActive = this.get('model.isActive');
@@ -37,41 +44,16 @@ export default Controller.extend({
         this.get('model.hasValidated').pushObject('isActive');
     },
 
-    _makeRequest(url) {
-        let headers = {};
-
-        // clear any previous error
-        this.set('error', '');
-
-        headers.Authorization = `apikey ${this.get('model.apiKey')}`;
-        // TODO: make request to mailchimp/lists enpoint with added _scratchApiKey
-        // return fetch(url, {headers})
-        //     .then((response) => response.json())
-        //     .then((response) => this._addListsFromResponse(response))
-        //     .catch(() => {
-        //         // if the error text isn't already set then we've get a connection error from `fetch`
-        //         if (!this.get('error')) {
-        //             this.set('error', 'Uh-oh! Trouble reaching the Unsplash API, please check your connection');
-        //         }
-        //     });
-    },
-
-    _addListsFromResponse(response) {
-        let lists = response.results || response;
-
-        this.set('isFetchingLists', false);
-
-        lists.forEach((list) => this.get('availableLists').pushObject(list));
-    },
-
     _fetchMailingLists: task(function* () {
-        let url = `${this.get('apiUrl')}/lists`;
+        let url = this.get('ghostPaths.url').api('mailchimp', 'lists');
+        let data = {apiKey: this.get('model.apiKey')};
 
         yield this.get('model').validate({property: 'apiKey'});
 
-        this.set('isFetchingLists', true);
-        yield this._makeRequest(url);
-        this.set('isFetchingLists', false);
+        // TODO: error handling
+        let result = yield this.get('ajax').request(url, {data});
+
+        this.set('availableLists', result.lists);
     }).drop(),
 
     save: task(function* () {
