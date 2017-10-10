0c52a0fbc1198cfe45bb4d83e13114b42ce62ecd
Aileen Nowak <AileenCGN@gmail.com>
2017-10-10 12:28:29 +0700

    🎨  Confirmation dialog for Apps - AMP
    refs TryGhost/Ghost#9119
    
    - Added `leave-settings-modal` component to Settings - Apps - AMP

--- app/routes/settings/apps/amp.js
+++ app/routes/settings/apps/amp.js
@@ -1,14 +1,29 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 import styleBody from 'ghost-admin/mixins/style-body';
+import {inject as injectService} from '@ember/service';
 
 export default AuthenticatedRoute.extend(styleBody, {
     titleToken: 'Settings - Apps - AMP',
 
     classNames: ['settings-view-apps-amp'],
 
+    settings: injectService(),
+
     actions: {
         save() {
             this.get('controller').send('save');
+        },
+
+        willTransition(transition) {
+            let controller = this.get('controller');
+            let settings = this.get('settings');
+            let modelIsDirty = settings.get('hasDirtyAttributes');
+
+            if (modelIsDirty) {
+                transition.abort();
+                controller.send('toggleLeaveSettingsModal', transition);
+                return;
+            }
         }
     }
 
