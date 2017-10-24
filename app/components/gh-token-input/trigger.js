ddd7b69f34fb2ebeaa845a92bf6f27ea3f14eb9e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-10-24 18:04:45 +0100

    fix update behaviour when re-ordering

--- app/components/gh-token-input/trigger.js
+++ app/components/gh-token-input/trigger.js
@@ -1,6 +1,8 @@
 import EmberPowerSelectMultipleTrigger from 'ember-power-select/components/power-select-multiple/trigger';
+import {copy} from '@ember/object/internals';
 
 export default EmberPowerSelectMultipleTrigger.extend({
+
     actions: {
         handleOptionMouseDown(event) {
             let action = this.get('extra.optionMouseDown');
@@ -14,6 +16,13 @@ export default EmberPowerSelectMultipleTrigger.extend({
             if (action) {
                 return action(event);
             }
+        },
+
+        reorderItems() {
+            // ember-drag-drop's sortable-objects has two-way bindings and will
+            // update EPS' selected value directly. We have to create a copy
+            // after sorting in order to force the onchange action to be triggered
+            this.get('select').actions.select(copy(this.get('select.selected')));
         }
     }
 });
