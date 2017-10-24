299db708113c27b3839f50ccfb21d84f83c9846a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-10-24 17:06:40 +0100

    disable focus on likely drag event to fix ember-drag-drop

--- app/components/gh-token-input/trigger.js
+++ app/components/gh-token-input/trigger.js
@@ -1,5 +1,19 @@
 import EmberPowerSelectMultipleTrigger from 'ember-power-select/components/power-select-multiple/trigger';
 
 export default EmberPowerSelectMultipleTrigger.extend({
+    actions: {
+        handleOptionMouseDown(event) {
+            let action = this.get('extra.optionMouseDown');
+            if (action) {
+                return action(event);
+            }
+        },
 
+        handleOptionTouchStart(event) {
+            let action = this.get('extra.optionTouchStart');
+            if (action) {
+                return action(event);
+            }
+        }
+    }
 });
