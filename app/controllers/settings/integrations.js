253e6ba1c49d12a53ac4f0751c8d8f64bee1b84d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-10-03 19:06:00 +0100

    display custom integrations list if dev experiments flag is set

--- app/controllers/settings/integrations.js
+++ app/controllers/settings/integrations.js
@@ -3,5 +3,6 @@ import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
 
 export default Controller.extend({
+    config: service(),
     settings: service()
 });
