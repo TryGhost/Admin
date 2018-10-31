0c17cb46bf88035998e201194727f3b6a57fa61b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-10-31 21:21:55 +0000

    switch to unreleased version of ember-power-datepicker with ember-test-helpers support

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,9 +1,6 @@
 import Application from '../app';
 import config from '../config/environment';
-import registerPowerDatepickerHelpers from '../tests/helpers/ember-power-datepicker';
 import {setApplication} from '@ember/test-helpers';
-
-registerPowerDatepickerHelpers();
 
 setApplication(Application.create(config.APP));
 
