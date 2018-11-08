b51e9b9ee8eb9b3e3b85a3b49e6d155c3a2ecc3b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-11-08 16:25:53 +0000

    update mirage development scenario to generate nested tags

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -5,7 +5,20 @@ export default function (server) {
     // server.createList('contact', 10);
 
     server.createList('subscriber', 125);
-    server.createList('tag', 100);
-
     server.create('integration', {name: 'Demo'});
+
+    server.createList('tag', 20);
+    server.schema.tags.all().models.forEach((tag) => {
+        if (Math.random() < 0.5) {
+            let number = Math.ceil(Math.random() * 6);
+            server.createList('tag', number, {parent: tag});
+        }
+    });
+    let tagsWithParents = server.schema.tags.all().filter(tag => !!tag.parent);
+    tagsWithParents.models.forEach((tag) => {
+        if (Math.random() < 0.5) {
+            let number = Math.ceil(Math.random() * 3);
+            server.createList('tag', number, {parent: tag});
+        }
+    });
 }
