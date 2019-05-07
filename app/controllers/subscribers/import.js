b1c1233a7302e6763dd59e142179ac0e9b5bb846
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-05-07 11:03:39 +0100

    fixed import modal

---
+++ app/controllers/subscribers/import.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+import {inject as service} from '@ember/service';
+
+/* eslint-disable ghost/ember/alias-model-in-controller */
+export default Controller.extend({
+    router: service(),
+
+    actions: {
+        close() {
+            this.router.transitionTo('subscribers');
+        }
+    }
+});
