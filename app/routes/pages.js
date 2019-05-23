b4633698edafe0275d7dd7c113f30f0d415efc81
Zimo <peter.zimon@gmail.com>
2019-05-23 17:20:27 +0200

    Changed pages background

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -5,8 +5,7 @@ export default PostsRoute.extend({
 
     buildRouteInfoMetadata() {
         return {
-            titleToken: 'Pages',
-            mainClasses: ['gh-main-white']
+            titleToken: 'Pages'
         };
     }
 });
