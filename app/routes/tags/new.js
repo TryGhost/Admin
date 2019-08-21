d5d8a8cca87958c32c0c1d34a743e6f4b6f03faf
Rish <zrishabhgarg@gmail.com>
2019-08-21 16:33:29 +0530

    Updated tag detail and new tag screens

--- app/routes/tags/new.js
+++ app/routes/tags/new.js
@@ -1,8 +1,18 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import {inject as service} from '@ember/service';
 
 export default AuthenticatedRoute.extend({
 
+    router: service(),
+
     controllerName: 'tags.tag',
+
+    init() {
+        this._super(...arguments);
+        this.router.on('routeWillChange', (transition) => {
+            this.showUnsavedChangesModal(transition);
+        });
+    },
 
     model() {
         return this.store.createRecord('tag');
@@ -19,6 +29,18 @@ export default AuthenticatedRoute.extend({
         let {controller} = this;
         controller.model.rollbackAttributes();
         controller.set('model', null);
+    },
+
+    showUnsavedChangesModal(transition) {
+        if (transition.from && transition.from.name.match(/^tags\.new/) && transition.targetName) {
+            let {controller} = this;
+
+            if (!controller.tag.isDeleted && controller.tag.hasDirtyAttributes) {
+                transition.abort();
+                controller.send('toggleUnsavedChangesModal', transition);
+                return;
+            }
+        }
     }
 
 });
