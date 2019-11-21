da77073acdf35392b474e51d656b5368f2412e5d
Nazar Gargol <nazargargol@gmail.com>
2019-11-21 17:35:24 +0700

    Added missing date serialization properties to member model
    - Without these the member model definition was not seeing correct fields

--- app/serializers/member.js
+++ app/serializers/member.js
@@ -2,6 +2,11 @@
 import ApplicationSerializer from 'ghost-admin/serializers/application';
 
 export default ApplicationSerializer.extend({
+    attrs: {
+        createdAtUTC: {key: 'created_at'},
+        updatedAtUTC: {key: 'updated_at'}
+    },
+
     serialize(/*snapshot, options*/) {
         let json = this._super(...arguments);
 
