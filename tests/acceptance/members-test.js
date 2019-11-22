8c6a373a6995c22f30e1b4209525d2e4353b3874
Nazar Gargol <nazargargol@gmail.com>
2019-11-22 16:38:20 +0700

    Added missing imports to members-test

--- tests/acceptance/members-test.js
+++ tests/acceptance/members-test.js
@@ -1,9 +1,12 @@
+import moment from 'moment';
+import wait from 'ember-test-helpers/wait';
 import {authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
 import {beforeEach, describe, it} from 'mocha';
-import {click, currentRouteName, currentURL, find} from '@ember/test-helpers';
+import {click, currentRouteName, currentURL, fillIn, find, findAll} from '@ember/test-helpers';
 import {expect} from 'chai';
 import {setupApplicationTest} from 'ember-mocha';
 import {setupMirage} from 'ember-cli-mirage/test-support';
+import {timeout} from 'ember-concurrency';
 import {visit} from '../helpers/visit';
 
 describe('Acceptance: Members', function () {
