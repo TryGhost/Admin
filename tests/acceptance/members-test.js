23e6934f271502a324f1513f0a968b725011fe09
Nazar Gargol <nazargargol@gmail.com>
2019-11-22 16:35:19 +0700

    Added members listing and editing acceptance test

--- tests/acceptance/members-test.js
+++ tests/acceptance/members-test.js
@@ -53,5 +53,55 @@ describe('Acceptance: Members', function () {
             expect(currentRouteName()).to.equal('members.index');
             expect(find('[data-test-screen-title]')).to.have.text('Members');
         });
+
+        it('it renders, can be navigated, can edit member', async function () {
+            let member1 = this.server.create('member', {createdAt: moment.utc().subtract(1, 'day').valueOf()});
+            this.server.create('member', {createdAt: moment.utc().subtract(2, 'day').valueOf()});
+
+            await visit('/members');
+
+            // second wait is needed for the vertical-collection to settle
+            await wait();
+
+            // lands on correct page
+            expect(currentURL(), 'currentURL').to.equal('/members');
+
+            // it has correct page title
+            expect(document.title, 'page title').to.equal('Members - Test Blog');
+
+            // it lists all members
+            expect(findAll('.members-list .gh-members-list-item').length, 'members list count')
+                .to.equal(2);
+
+            let member = find('.members-list .gh-members-list-item');
+            expect(member.querySelector('.gh-members-list-name').textContent, 'member list item title')
+                .to.equal(member1.name);
+
+            await visit(`/members/${member1.id}`);
+
+            // // second wait is needed for the member details to settle
+            await wait();
+
+            // it shows selected member form
+            expect(find('.gh-member-basic-settings-form input[name="name"]').value, 'loads correct member into form')
+                .to.equal(member1.name);
+
+            // trigger save
+            await fillIn('.gh-member-basic-settings-form input[name="name"]', 'New Name');
+            await blur('.gh-member-basic-settings-form input[name="name"]');
+
+            await click('[data-test-button="save"]');
+
+            // // extra timeout needed for Travis - sometimes it doesn't update
+            // // quick enough and an extra wait() call doesn't help
+            await timeout(100);
+
+            await click('[data-test-link="members-back"]');
+
+            await wait();
+
+            // lands on correct page
+            expect(currentURL(), 'currentURL').to.equal('/members');
+        });
     });
 });
