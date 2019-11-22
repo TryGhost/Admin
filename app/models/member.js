9e1c8a3f90934f31a92886215b785ae166d4478d
Nazar Gargol <nazargargol@gmail.com>
2019-11-22 10:35:51 +0700

    Added email validation to member email

--- app/models/member.js
+++ app/models/member.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
+import ValidationEngine from 'ghost-admin/mixins/validation-engine';
 import attr from 'ember-data/attr';
 
-export default DS.Model.extend({
+export default DS.Model.extend(ValidationEngine, {
+    validationType: 'member',
+
     name: attr('string'),
     email: attr('string'),
     note: attr('string'),
---
+++ app/validators/member.js
@@ -0,0 +1,21 @@
+import BaseValidator from './base';
+import validator from 'validator';
+import {isBlank} from '@ember/utils';
+
+export default BaseValidator.create({
+    properties: ['email'],
+
+    email(model) {
+        let email = model.get('email');
+
+        if (isBlank(email)) {
+            model.get('errors').add('email', 'Please enter an email.');
+            this.invalidate();
+        } else if (!validator.isEmail(email)) {
+            model.get('errors').add('email', 'Invalid Email.');
+            this.invalidate();
+        }
+
+        model.get('hasValidated').addObject('email');
+    }
+});
