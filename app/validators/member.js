cada626869082b3a22d3fad65163226a9e0e6135
Nazar Gargol <nazargargol@gmail.com>
2019-11-22 11:58:22 +0700

    Added missing validations in members form

--- app/validators/member.js
+++ app/validators/member.js
@@ -3,7 +3,14 @@ import validator from 'validator';
 import {isBlank} from '@ember/utils';
 
 export default BaseValidator.create({
-    properties: ['email'],
+    properties: ['name', 'email', 'note'],
+
+    name(model) {
+        if (!validator.isLength(model.name || '', 0, 191)) {
+            model.errors.add('name', 'Name cannot be longer than 191 characters.');
+            this.invalidate();
+        }
+    },
 
     email(model) {
         let email = model.get('email');
@@ -15,7 +22,22 @@ export default BaseValidator.create({
             model.get('errors').add('email', 'Invalid Email.');
             this.invalidate();
         }
+        if (!validator.isLength(model.name || '', 0, 191)) {
+            model.errors.add('email', 'Email cannot be longer than 191 characters.');
+            this.invalidate();
+        }
 
         model.get('hasValidated').addObject('email');
+    },
+
+    note(model) {
+        let note = model.get('note');
+
+        if (!validator.isLength(note || '', 0, 500)) {
+            model.get('errors').add('note', 'Note is too long.');
+            this.invalidate();
+        }
+
+        model.get('hasValidated').addObject('note');
     }
 });
