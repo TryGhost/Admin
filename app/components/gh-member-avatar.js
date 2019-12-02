59bf527e0b6d26816fa91ce65fa395a94e934645
Nazar Gargol <nazargargol@gmail.com>
2019-12-02 18:03:36 +0700

    Extracted member gravatar calculation into a helper
    - Helper is reusable and could be applied in other places
    - Aslo solves a problem of flickering images when loading a component

--- app/components/gh-member-avatar.js
+++ app/components/gh-member-avatar.js
@@ -1,11 +1,7 @@
 import Component from '@ember/component';
-import md5 from 'blueimp-md5';
-import request from 'ember-ajax/request';
-import validator from 'validator';
 import {computed} from '@ember/object';
 import {htmlSafe} from '@ember/string';
 import {inject as service} from '@ember/service';
-import {task} from 'ember-concurrency';
 
 const stringToHslColor = function (str, saturation, lightness) {
     var hash = 0;
@@ -21,12 +17,8 @@ export default Component.extend({
     config: service(),
 
     tagName: '',
-    email: '',
-    size: 180,
 
     member: null,
-    placeholderStyle: htmlSafe('background-image: url()'),
-    avatarStyle: htmlSafe('display: none'),
 
     initialsClass: computed('sizeClass', function () {
         return this.sizeClass || 'gh-member-list-avatar';
@@ -52,45 +44,5 @@ export default Component.extend({
 
         // New Member initials
         return 'NM';
-    }),
-
-    didReceiveAttrs() {
-        this._super(...arguments);
-
-        if (this.get('config.useGravatar')) {
-            this.setGravatar.perform();
-        }
-    },
-
-    setGravatar: task(function* () {
-        let email = this.member.email;
-
-        if (validator.isEmail(email || '')) {
-            let size = this.size;
-            let gravatarUrl = `//www.gravatar.com/avatar/${md5(email)}?s=${size}&d=blank`;
-
-            try {
-                // HEAD request is needed otherwise jquery attempts to process
-                // binary data as JSON and throws an error
-                yield request(gravatarUrl, {type: 'HEAD'});
-                // gravatar exists so switch style and let browser load it
-                this._setAvatarImage(gravatarUrl);
-                this._setPlaceholderImage('');
-            } catch (e) {
-                // gravatar doesn't exist so make sure we're still showing the placeholder
-                this._setPlaceholderImage(this._defaultImageUrl);
-                // then make sure the avatar isn't visible
-                this._setAvatarImage('');
-            }
-        }
-    }).restartable(),
-
-    _setPlaceholderImage(url) {
-        this.set('placeholderStyle', htmlSafe(`background-image: url(${url});`));
-    },
-
-    _setAvatarImage(url) {
-        let display = url ? 'block' : 'none';
-        this.set('avatarStyle', htmlSafe(`background-image: url(${url}); display: ${display}`));
-    }
+    })
 });
