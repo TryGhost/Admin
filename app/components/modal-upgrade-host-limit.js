f845fa348ffb9999ac4bce0909103fe9d3d71233
Rish <zrishabhgarg@gmail.com>
2019-12-16 22:03:29 +0530

    Added error details from server to build upgrade message

--- app/components/modal-upgrade-host-limit.js
+++ app/components/modal-upgrade-host-limit.js
@@ -1,8 +1,16 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
+import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default ModalComponent.extend({
     session: service(),
+    upgradeMessage: computed('details', function () {
+        const errorDetails = this.model.details;
+        return {
+            limit: errorDetails.limit,
+            total: errorDetails.attempted
+        };
+    }),
     actions: {
         upgrade: function () {
             const upgradeLink = this.model.upgradeLink;
