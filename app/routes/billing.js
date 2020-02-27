2dcc1549ea8e5fdc476c0e6d68c349f849422ce1
Rish <zrishabhgarg@gmail.com>
2020-02-27 09:18:04 +0530

    Transition to home route for missing billing config
    no issue

--- app/routes/billing.js
+++ app/routes/billing.js
@@ -1,6 +1,16 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import {inject as service} from '@ember/service';
 
 export default AuthenticatedRoute.extend({
+    config: service(),
+    router: service(),
+
+    beforeModel() {
+        if (!this.get('config.billingUrl')) {
+            return this.transitionTo('home');
+        }
+    },
+
     model() {
         return (new Date()).valueOf();
     },
