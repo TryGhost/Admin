17221f3a12ae85f75ca15bb575f459cdff284701
Rish <zrishabhgarg@gmail.com>
2020-05-05 22:27:19 +0530

    Added error handling for failed refresh

--- app/components/modal-regenerate-key.js
+++ app/components/modal-regenerate-key.js
@@ -1,5 +1,6 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {alias} from '@ember/object/computed';
+import {capitalize} from '@ember/string';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
@@ -18,23 +19,20 @@ export default ModalComponent.extend({
         }
     },
     regenerateKey: task(function* () {
+        let url = this.get('ghostPaths.url').api('/integrations/', this.integration.id, 'api_key', this.apiKey.id, 'refresh');
         try {
-            let url = this.get('ghostPaths.url').api('/integrations/', this.integration.id, 'api_key', this.apiKey.id, 'refresh');
-            try {
-                const response = yield this.ajax.post(url, {
-                    data: {
-                        integrations: [{id: this.integration.id}]
-                    }
-                });
-                this.store.pushPayload(response);
-                yield this.confirm();
-            } catch (e) {
-                let errMessage = 'There was an error regenerating the Admin API Key. Please try again';
-                this.set('errorMessage', errMessage);
-                return;
-            }
-        } finally {
+            const response = yield this.ajax.post(url, {
+                data: {
+                    integrations: [{id: this.integration.id}]
+                }
+            });
+            this.store.pushPayload(response);
+            yield this.confirm();
             this.send('closeModal');
+        } catch (e) {
+            let errMessage = `There was an error regenerating the ${capitalize(this.apiKey.type)} API Key. Please try again`;
+            this.set('errorMessage', errMessage);
+            return;
         }
     }).drop()
 });
