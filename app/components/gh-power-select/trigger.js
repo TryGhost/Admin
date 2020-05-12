bebbb87936ca76d7652df5c023d3a76bc1d8b5b3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-05-12 09:49:52 +0100

    Fixed trigger component override collision when building
    - move the "override" into our own namespace
    - update all `<PowerSelect>` usage to explicitly reference our customised trigger component

--- app/components/gh-power-select/trigger.js
+++ app/components/gh-power-select/trigger.js
@@ -1,4 +1,4 @@
-import layout from '../../templates/components/power-select/trigger';
+import layout from '../../templates/components/gh-power-select/trigger';
 import trigger from 'ember-power-select/components/power-select/trigger';
 
 export default trigger.extend({
