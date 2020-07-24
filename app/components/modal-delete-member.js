332e59b7233d1baa96fb97ef7730d5b6873f52ee
Fabien O'Carroll <fabien@allou.is>
2020-07-24 11:35:47 +0200

    Supported cancellation of subscriptions on delete
    no-issue
    
    This makes the cancellation of subscriptions much more obvious to the
    user, and we err on the side of caution by *not* cancelling by default.

--- app/components/modal-delete-member.js
+++ app/components/modal-delete-member.js
@@ -1,25 +1,46 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {alias} from '@ember/object/computed';
+import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
 export default ModalComponent.extend({
     membersStats: service(),
 
+    cancelSubscriptions: false,
+
     // Allowed actions
     confirm: () => {},
 
     member: alias('model'),
 
+    hasActiveStripeSubscriptions: computed('member', function () {
+        let subscriptions = this.member.get('stripe');
+
+        if (!subscriptions || subscriptions.length === 0) {
+            return false;
+        }
+
+        let firstActiveStripeSubscription = subscriptions.find((subscription) => {
+            return subscription.status === 'active' || subscription.status === 'trialing';
+        });
+
+        return firstActiveStripeSubscription !== undefined;
+    }),
+
     actions: {
         confirm() {
             this.deleteMember.perform();
+        },
+
+        toggleCancelSubscriptions() {
+            this.cancelSubscriptions = !this.cancelSubscriptions;
         }
     },
 
     deleteMember: task(function* () {
         try {
-            yield this.confirm();
+            yield this.confirm(this.cancelSubscriptions);
             this.membersStats.invalidate();
         } finally {
             this.send('closeModal');
