627c2253c25d07de9e32acfe1552a2c026457855
Peter Zimon <zimo@ghost.org>
2020-11-04 12:40:13 +0100

    Added empty email setting popup

---
+++ app/components/modal-email-design-settings.js
@@ -0,0 +1,10 @@
+import ModalComponent from 'ghost-admin/components/modal-base';
+
+export default ModalComponent.extend({
+
+    init() {
+        this._super(...arguments);
+    },
+
+    actions: {}
+});
