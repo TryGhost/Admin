78a62129b4add9e6dd24695e67177c57f64d0252
Peter Zimon <zimo@ghost.org>
2020-11-05 15:44:17 +0100

    Added naive UI logic

--- app/components/modal-email-design-settings.js
+++ app/components/modal-email-design-settings.js
@@ -2,9 +2,30 @@ import ModalComponent from 'ghost-admin/components/modal-base';
 
 export default ModalComponent.extend({
 
+    showHeader: true,
+    showSansSerif: false,
+    showBadge: true,
+    footerText: '',
+
     init() {
         this._super(...arguments);
     },
 
-    actions: {}
+    actions: {
+        toggleShowHeader(showHeader) {
+            this.set('showHeader', showHeader);
+        },
+
+        setTypography(typography) {
+            if (typography === 'serif') {
+                this.set('showSansSerif', false);
+            } else {
+                this.set('showSansSerif', true);
+            }
+        },
+
+        toggleBadge(showBadge) {
+            this.set('showBadge', showBadge);
+        }
+    }
 });
