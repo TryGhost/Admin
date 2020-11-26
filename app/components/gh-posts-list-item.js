b50af6f17c546189a9b7e77f3597af2054503a07
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-11-26 11:01:59 +0000

    add hover state for posts list item

--- app/components/gh-posts-list-item.js
+++ app/components/gh-posts-list-item.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
+import {action} from '@ember/object';
 import {formatPostTime} from 'ghost-admin/helpers/gh-format-post-time';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class GhPostsListItemComponent extends Component {
     @service session;
     @service settings;
+
+    @tracked isHovered = false;
 
     get authorNames() {
         return this.args.post.authors.map(author => author.name || author.email).join(', ');
@@ -26,5 +30,15 @@ export default class GhPostsListItemComponent extends Component {
         text.push(formattedTime);
 
         return text.join(' ');
+    }
+
+    @action
+    mouseOver() {
+        this.isHovered = true;
+    }
+
+    @action
+    mouseLeave() {
+        this.isHovered = false;
     }
 }
