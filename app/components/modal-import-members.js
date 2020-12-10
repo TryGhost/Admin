5184b80b88d444e65899cf9a389e813affffa533
Peter Zimon <zimo@ghost.org>
2020-12-10 11:10:46 +0100

    Updated UI logic for no-member upload case

--- app/components/modal-import-members.js
+++ app/components/modal-import-members.js
@@ -132,23 +132,23 @@ export default ModalComponent.extend({
             const formattedError = row.error
                 .replace(
                     'Value in [members.email] cannot be blank.',
-                    'Missing email address'
+                    'Missing email address.'
                 )
                 .replace(
                     'Value in [members.note] exceeds maximum length of 2000 characters.',
-                    '"Note" exceeds maximum length of 2000 characters'
+                    'Note is too long.'
                 )
                 .replace(
                     'Value in [members.subscribed] must be one of true, false, 0 or 1.',
-                    'Value in "Subscribed to emails" must be "true" or "false"'
+                    'Value of "Subscribed to emails" must be "true" or "false"'
                 )
                 .replace(
                     'Validation (isEmail) failed for email',
-                    'Invalid email address'
+                    'Invalid email address.'
                 )
                 .replace(
                     /No such customer:[^,]*/,
-                    'Could not find Stripe customer'
+                    'Could not find Stripe customer.'
                 );
             formattedError.split(',').forEach((errorMssg) => {
                 if (errorList[errorMssg]) {
