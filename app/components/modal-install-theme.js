14d3180e792f65beff6b3301cdd9be2f9b6fcde4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-11 11:51:00 +0000

    switched to manual activation after install

--- app/components/modal-install-theme.js
+++ app/components/modal-install-theme.js
@@ -21,20 +21,32 @@ export default class ModalInstallThemeComponent extends ModalBase {
         return this.model.ref.split('/')[1];
     }
 
-    get willOverwriteDefault() {
-        return this.themeName.toLowerCase() === 'casper';
-    }
-
     get currentThemeNames() {
         return this.model.themes.mapBy('name');
     }
 
+    get willOverwriteDefault() {
+        return this.themeName.toLowerCase() === 'casper';
+    }
+
     get willOverwrite() {
-        return !!this.model.themes.findBy('name', this.themeName.toLowerCase());
+        return this.model.themes.findBy('name', this.themeName.toLowerCase());
     }
 
     get installSuccess() {
         return !!this.theme;
+    }
+
+    get shouldShowInstall() {
+        return !this.installSuccess && !this.willOverwriteDefault;
+    }
+
+    get shouldShowActivate() {
+        return this.installSuccess && !this.theme.active;
+    }
+
+    get hasActionButton() {
+        return this.shouldShowInstall || this.shouldShowActivate;
     }
 
     @action
@@ -54,35 +66,24 @@ export default class ModalInstallThemeComponent extends ModalBase {
     }
 
     @task({drop: true})
-    *install() {
+    *installTask() {
         // TODO: update API to use PUT/POST
         const url = this.ghostPaths.url.api('themes/install') + `?source=github&ref=${this.model.ref}`;
         const result = yield this.ajax.request(url);
 
         if (result.themes) {
-            const [theme] = result.themes;
-
-            this.theme = theme;
-
             // show theme in list immediately
             this.store.pushPayload(result);
 
-            // deactivate any other active themes
-            // (this happens on the server but we only get the active theme back so we need to do this manually)
-            if (theme.active) {
-                const activeThemes = this.store.peekAll('theme').filterBy('active', true);
-                activeThemes.forEach((themeModel) => {
-                    if (themeModel.name !== theme.name) {
-                        // store.push is necessary to avoid dirty records that cause
-                        // problems when we get new data back in subsequent requests
-                        this.store.push({data: {
-                            id: themeModel.id,
-                            type: 'theme',
-                            attributes: {active: false}
-                        }});
-                    }
-                });
-            }
+            this.theme = this.store.peekRecord('theme', result.themes[0].name);
+
+            return true;
         }
+    }
+
+    @task({drop: true})
+    *activateTask() {
+        yield this.theme.activate();
+        this.closeModal();
     }
 }
