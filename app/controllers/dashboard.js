0d32aeb544f2d74ac1cc7cfa2425b76a358ba68b
Fabien O'Carroll <fabien@allou.is>
2021-02-17 15:38:11 +0000

    Updated dashboard controller to load event timeline data

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,7 +1,21 @@
 import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class DashboardController extends Controller {
     @service feature;
     @service session;
+    @service membersStats;
+
+    @tracked
+    events = null;
+
+    constructor(...args) {
+        super(...args);
+        this.membersStats.fetchTimeline().then(({events}) => {
+            this.events = events;
+        }, (error) => {
+            console.error(error);
+        });
+    }
 }
