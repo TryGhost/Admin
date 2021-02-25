6f393368e36c87976857a9ce74ef4cda0ce8b036
Fabien O'Carroll <fabien@allou.is>
2021-02-25 09:36:33 +0000

    Updated currency utils

--- app/utils/currency.js
+++ app/utils/currency.js
@@ -121,30 +121,10 @@ export const currencies = [
 ];
 
 export function getSymbol(currency) {
-    switch (currency) {
-    case 'usd':
-    case 'aud':
-    case 'cad':
-        return '$';
-    case 'eur':
-        return '€';
-    case 'gbp':
-        return '£';
-    case 'inr':
-        return '₹';
-    }
-    return null;
+    return Intl.NumberFormat('en', {currency, style: 'currency'}).format(0).replace(/[\d\s.]/g, '');
 }
 
-export function getNonDecimal(amount, currency) {
-    switch (currency) {
-    case 'usd':
-    case 'aud':
-    case 'cad':
-    case 'eur':
-    case 'gbp':
-    case 'inr':
-        return amount / 100;
-    }
-    return null;
+// We currently only support decimal currencies
+export function getNonDecimal(amount/*, currency*/) {
+    return amount / 100;
 }
