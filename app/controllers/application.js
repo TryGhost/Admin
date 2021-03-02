25237438b17c42cc493b3f69011599c5291de81d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-02 18:32:18 +0000

    Fixed update link not including token
    no issue
    
    - the Ghost(Pro) update app requires a token to be passed to identify the site
    - adds a click handler to the update banner link that fetches the identity token and appends it to the update url. Opens a blank tab immediately to link the tab open with a user action so we can avoid problems with popup blockers if the identity request is slow for any reason

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,12 +1,15 @@
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
 import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default Controller.extend({
-    customViews: service(),
+    ajax: service(),
     config: service(),
+    customViews: service(),
     dropdown: service(),
+    ghostPaths: service(),
     router: service(),
     session: service(),
     settings: service(),
@@ -30,5 +33,20 @@ export default Controller.extend({
 
         return (router.currentRouteName !== 'error404' || session.isAuthenticated)
                 && !router.currentRouteName.match(/(signin|signup|setup|reset)/);
+    }),
+
+    openUpdateTab: action(function () {
+        const updateWindow = window.open('', '_blank');
+
+        updateWindow.document.write('Loading...');
+
+        const updateUrl = new URL(this.config.get('updateUrl'));
+        const ghostIdentityUrl = this.ghostPaths.url.api('identities');
+
+        this.ajax.request(ghostIdentityUrl).then((response) => {
+            const token = response && response.identities && response.identities[0] && response.identities[0].token;
+            updateUrl.searchParams.append('jwt', token);
+            updateWindow.location.href = updateUrl.toString();
+        });
     })
 });
