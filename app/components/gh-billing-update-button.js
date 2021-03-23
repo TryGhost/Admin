fb7010e20964977557186a638e9beca1324cf5be
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-23 11:59:20 +0000

    Revert "fixed upgrade button link"
    This reverts commit 0da397eee6ba8713c4cc681c88c09bfb911839f5.
    
    I was running an old version of the billing app locally

--- app/components/gh-billing-update-button.js
+++ app/components/gh-billing-update-button.js
@@ -15,7 +15,7 @@ export default Component.extend({
 
     actions: {
         openBilling() {
-            this.billing.openBillingWindow(this.router.currentURL, '/pro/plans');
+            this.billing.openBillingWindow(this.router.currentURL, '/pro/billing/plans');
         }
     }
 });
