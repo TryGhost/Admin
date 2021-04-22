ff373e321fed848bee330067deb510f1eefc51ec
Rish <zrishabhgarg@gmail.com>
2021-04-22 19:52:36 +0530

    Wired read only data on products page UI
    refs https://github.com/TryGhost/Team/issues/627
    
    Wires up the products data from API to Products and Prices UI pages

--- app/components/gh-products-price-billingperiod.js
+++ app/components/gh-products-price-billingperiod.js
@@ -1,13 +1,22 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import {inject as service} from '@ember/service';
 
 const PERIODS = [
-    {label: 'Monthly', period: 'monthly'},
-    {label: 'Yearly', period: 'yearly'}
+    {label: 'Monthly', period: 'month'},
+    {label: 'Yearly', period: 'year'}
 ];
 
-export default Component.extend({
-    init() {
-        this._super(...arguments);
+export default class GhPostsListItemComponent extends Component {
+    @service feature;
+    @service session;
+    @service settings;
+
+    get value() {
+        const {value} = this.args;
+        return value;
+    }
+    constructor() {
+        super(...arguments);
         this.availablePeriods = PERIODS;
     }
-});
+}
