2bd06debb4a819e0c95c974b6c4f9241b2edb7cb
Rishabh <rish@ghost.org>
2021-05-07 15:53:14 +0530

    Added test files for product

---
+++ mirage/config/products.js
@@ -0,0 +1,50 @@
+import moment from 'moment';
+import { Response } from 'ember-cli-mirage';
+import { paginateModelCollection } from '../utils';
+
+export default function mockPosts(server) {
+    server.post('/products', function ({ products }) {
+        let attrs = this.normalizedRequestAttrs();
+
+        return products.create(attrs);
+    });
+
+    // TODO: handle authors filter
+    server.get('/products/', function ({ products }, { queryParams }) {
+        let { page, limit } = queryParams;
+
+        page = +page || 1;
+        limit = +limit || 15;
+
+        let collection = products.all().filter(() => {
+            let matchesStatus = true;
+            return matchesStatus;
+        });
+
+        return paginateModelCollection('products', collection, page, limit);
+    });
+
+    server.get('/products/:id/', function ({ products }, { params }) {
+        let { id } = params;
+        let product = products.find(id);
+
+        return product || new Response(404, {}, {
+            errors: [{
+                type: 'NotFoundError',
+                message: 'product not found.'
+            }]
+        });
+    });
+
+    server.put('/products/:id/', function ({ products }, { params }) {
+        let attrs = this.normalizedRequestAttrs();
+        let product = products.find(params.id);
+
+        attrs.updatedAt = moment.utc().toDate();
+
+        return product.update(attrs);
+    });
+
+    server.del('/products/:id/');
+}
+
---
+++ mirage/factories/product.js
@@ -0,0 +1,15 @@
+import faker from 'faker';
+import moment from 'moment';
+import { Factory, trait } from 'ember-cli-mirage';
+
+let randomDate = function randomDate(start = moment().subtract(30, 'days').toDate(), end = new Date()) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+
+export default Factory.extend({
+    name() { return `${faker.name.firstName()} ${faker.name.lastName()}`; },
+    description(i) { return `Title for product ${i}.`; },
+    slug(i) { return `product-${i}`; },
+    createdAt() { return randomDate(); }
+});
+
