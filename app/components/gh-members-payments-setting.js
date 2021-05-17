26472e1f0c13d385b0b6d3a15dd07b26c680801e
Rishabh <zrishabhgarg@gmail.com>
2021-05-17 13:46:12 +0530

    Fixed lint

--- app/components/gh-members-payments-setting.js
+++ app/components/gh-members-payments-setting.js
@@ -1,7 +1,7 @@
 import Component from '@ember/component';
+import envConfig from 'ghost-admin/config/environment';
 import {computed} from '@ember/object';
 import {currencies} from 'ghost-admin/utils/currency';
-import envConfig from 'ghost-admin/config/environment';
 import {reads} from '@ember/object/computed';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
