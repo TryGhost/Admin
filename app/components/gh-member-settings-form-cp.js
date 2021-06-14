f381c979c3995f2b10225ccca298fc07e2953e00
Rishabh <zrishabhgarg@gmail.com>
2021-06-15 00:39:35 +0530

    Cleanup

--- app/components/gh-member-settings-form-cp.js
+++ app/components/gh-member-settings-form-cp.js
@@ -30,9 +30,9 @@ export default class extends Component {
     }
 
     get isAddComplimentaryAllowed() {
-        // if (!this.membersUtils.isStripeEnabled) {
-        //     return false;
-        // }
+        if (!this.membersUtils.isStripeEnabled) {
+            return false;
+        }
         let subscriptions = this.member.get('subscriptions') || [];
         const hasZeroPriceSub = subscriptions.filter((sub) => {
             return ['active', 'trialing', 'unpaid', 'past_due'].includes(sub.status);
--- app/components/modal-member-product.js
+++ app/components/modal-member-product.js
@@ -1,6 +1,5 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {action} from '@ember/object';
-import {getNonDecimal, getSymbol} from 'ghost-admin/utils/currency';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency-decorators';
 import {tracked} from '@glimmer/tracking';
@@ -34,8 +33,13 @@ export default class ModalMemberProduct extends ModalComponent {
 
     async fetchProducts() {
         this.products = await this.store.query('product', {include: 'monthly_price,yearly_price'});
-        this.product = this.products.firstObject;
-        // this.price = this.prices ? this.prices[0] : null;
+    }
+
+    get activeSubscriptions() {
+        const subscriptions = this.member.get('subscriptions') || [];
+        return subscriptions.filter((sub) => {
+            return ['active', 'trialing', 'unpaid', 'past_due'].includes(sub.status);
+        });
     }
 
     get member() {
@@ -45,42 +49,6 @@ export default class ModalMemberProduct extends ModalComponent {
     get cannotAddPrice() {
         return !this.price || this.price.amount !== 0;
     }
-
-    // get prices() {
-    //     if (!this.products || !this.products.length) {
-    //         return [];
-    //     }
-    //     if (this.product) {
-    //         let subscriptions = this.member.get('subscriptions') || [];
-    //         let activeCurrency;
-    //         if (subscriptions.length > 0) {
-    //             activeCurrency = subscriptions[0].price?.currency;
-    //         }
-
-    //         const product = this.products.find((_product) => {
-    //             return _product.id === this.product.id;
-    //         });
-    //         return product.stripePrices.sort((a, b) => {
-    //             return a.amount - b.amount;
-    //         }).filter((price) => {
-    //             return price.active;
-    //         }).filter((price) => {
-    //             if (activeCurrency) {
-    //                 return price.currency?.toLowerCase() === activeCurrency.toLowerCase();
-    //             }
-    //             return true;
-    //         }).sort((a, b) => {
-    //             return a.currency.localeCompare(b.currency, undefined, {ignorePunctuation: true});
-    //         }).map((price) => {
-    //             return {
-    //                 ...price,
-    //                 label: `${price.nickname} (${getSymbol(price.currency)}${getNonDecimal(price.amount)}/${price.interval})`
-    //             };
-    //         });
-    //     } else {
-    //         return [];
-    //     }
-    // }
 
     @action
     setProduct(productId) {
@@ -97,12 +65,11 @@ export default class ModalMemberProduct extends ModalComponent {
     })
     *addProduct() {
         let url = this.ghostPaths.url.api(`members/${this.member.get('id')}`);
-
         let response = yield this.ajax.put(url, {
             data: {
                 members: [{
-                    id: this.member.id,
-                    email: this.member.email,
+                    id: this.member.get('id'),
+                    email: this.member.get('email'),
                     products: [{
                         id: this.selectedProduct
                     }]
