609baae0547f4afac14150ab0762da3570294188
Jose Bieco <jo_bieco@hotmail.com>
2021-10-22 08:32:11 -0300

    feat(app/components)gh-members-filter-value-labs.js

--- app/components/gh-members-filter-value-labs.js
+++ app/components/gh-members-filter-value-labs.js
@@ -1,37 +1,36 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
 import {tracked} from '@glimmer/tracking';
-
-const FILTER_OPTIONS = {
-    subscriptionPriceInterval: [
-        {label: 'Monthly', name: 'month'},
-        {label: 'Yearly', name: 'year'}
-    ],
-    status: [
-        {label: 'Paid', name: 'paid'},
-        {label: 'Free', name: 'free'},
-        {label: 'Complimentary', name: 'comped'}
-    ],
-    subscribed: [
-        {label: 'Subscribed', name: 'true'},
-        {label: 'Unsubscribed', name: 'false'}
-    ],
-    subscriptionStripeStatus: [
-        {label: 'Active', name: 'active'},
-        {label: 'Trialing', name: 'trialing'},
-        {label: 'Canceled', name: 'canceled'},
-        {label: 'Unpaid', name: 'unpaid'},
-        {label: 'Past Due', name: 'past_due'},
-        {label: 'Incomplete', name: 'incomplete'},
-        {label: 'Incomplete - Expired', name: 'incomplete_expired'}
-    ]
-};
-
 export default class GhMembersFilterValueLabs extends Component {
+    @service intl
     @tracked filterValue;
     constructor(...args) {
         super(...args);
-        this.availableFilterOptions = FILTER_OPTIONS;
+        this.availableFilterOptions = {
+            subscriptionPriceInterval: [
+                {label: this.intl.t('Manual.JS.Monthly'), name: 'month'},
+                {label: this.intl.t('Manual.JS.Yearly'), name: 'year'}
+            ],
+            status: [
+                {label: this.intl.t('Manual.JS.Paid'), name: 'paid'},
+                {label: this.intl.t('Manual.JS.Free'), name: 'free'},
+                {label: this.intl.t('Manual.JS.Complimentary'), name: 'comped'}
+            ],
+            subscribed: [
+                {label: this.intl.t('Manual.JS.Subscribed'), name: 'true'},
+                {label: this.intl.t('Manual.JS.Unsubscribed'), name: 'false'}
+            ],
+            subscriptionStripeStatus: [
+                {label: this.intl.t('Manual.JS.Active'), name: 'active'},
+                {label: this.intl.t('Manual.JS.Trialing'), name: 'trialing'},
+                {label: this.intl.t('Manual.JS.Canceled'), name: 'canceled'},
+                {label: this.intl.t('Manual.JS.Unpaid'), name: 'unpaid'},
+                {label: this.intl.t('Manual.JS.Past_Due'), name: 'past_due'},
+                {label: this.intl.t('Manual.JS.Incomplete'), name: 'incomplete'},
+                {label: this.intl.t('Manual.JS.Incomplete_Expired'), name: 'incomplete_expired'}
+            ]
+        };
         this.filterValue = this.args.filter.value;
     }
 
