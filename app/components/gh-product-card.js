b616968ba70194d61e0a89073e531e0a00c8f412
Rishabh <zrishabhgarg@gmail.com>
2022-01-14 13:49:44 +0530

    Updated currency code handling for product card
    refs https://github.com/TryGhost/Team/issues/1037

--- app/components/gh-product-card.js
+++ app/components/gh-product-card.js
@@ -1,8 +1,8 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
+import {getSymbol} from 'ghost-admin/utils/currency';
 import {inject as service} from '@ember/service';
 import {tracked} from '@glimmer/tracking';
-
 export default class extends Component {
     @service membersUtils;
     @service ghostPaths;
@@ -19,6 +19,11 @@ export default class extends Component {
 
     get isPaidProduct() {
         return this.product.type === 'paid';
+    }
+
+    get hasCurrencySymbol() {
+        const currencySymbol = getSymbol(this.product?.monthlyPrice?.currency);
+        return currencySymbol?.length !== 3;
     }
 
     get isFreeProduct() {
