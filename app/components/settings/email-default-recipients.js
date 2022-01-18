0f9b241dfd3c2daaaca953fde6bfb04c0bd7e963
Sanne de Vries <sannedv@protonmail.com>
2022-01-18 14:11:29 +0000

    Improved Email newsletter settings pag
    No ref
    
    - Added toggle to disable email newsletters
    - Rearranged the settings
    - Added publish-menu preview
    - Changed default-recipients setting

--- app/components/settings/email-default-recipients.js
+++ app/components/settings/email-default-recipients.js
@@ -1,62 +1,75 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
 
 export default class SettingsDefaultEmailRecipientsComponent extends Component {
     @service settings;
 
-    @tracked segmentSelected = false;
-
-    get isDisabled() {
-        return this.settings.get('membersSignupAccess') === 'none';
+    get options() {
+        return [{
+            name: 'Whoever has access to the post',
+            description: 'Free posts to everyone, premium posts sent to paid members',
+            value: 'visibility',
+            icon: 'members-post',
+            icon_color: 'green'
+        }, {
+            name: 'All members',
+            description: 'Everyone who is subscribed to newsletter updates, whether free or paid members',
+            value: 'invite',
+            icon: 'members-all',
+            icon_color: 'blue'
+        }, {
+            name: 'Paid-members only',
+            description: 'People who have a premium subscription',
+            value: 'none',
+            icon: 'members-paid',
+            icon_color: 'pink'
+        }, {
+            name: 'Specific tier(s)',
+            description: 'Only people who have a subscription to a selected tier',
+            value: 'segment',
+            icon: 'members-segment',
+            icon_color: 'yellow'
+        }, {
+            name: 'Usually nobody',
+            description: 'Newsletters are off for new posts, but can be enabled when needed',
+            value: 'none',
+            icon: 'no-members',
+            icon_color: 'midlightgrey-d2'
+        }, {
+            name: 'Disable completely',
+            description: 'Posts cannot be sent by email, deactivate newsletter features',
+            value: 'disabled',
+            icon: 'no-email',
+            icon_color: 'red'
+        }];
     }
 
-    get isDisabledSelected() {
-        return this.isDisabled ||
-            this.settings.get('editorDefaultEmailRecipients') === 'disabled';
-    }
-
-    get isVisibilitySelected() {
-        return !this.isDisabled &&
-            this.settings.get('editorDefaultEmailRecipients') === 'visibility';
-    }
-
-    get isNobodySelected() {
-        return !this.isDisabled &&
-            !this.segmentSelected &&
-            this.settings.get('editorDefaultEmailRecipients') === 'filter' &&
-            this.settings.get('editorDefaultEmailRecipientsFilter') === null;
-    }
-
-    get isSegmentSelected() {
-        const isCustomSegment = this.settings.get('editorDefaultEmailRecipients') === 'filter' &&
-            !this.isNobodySelected;
-        return !this.isDisabled && (this.segmentSelected || isCustomSegment);
+    get selectedOption() {
+        return this.options.find(o => o.value === this.settings.get('membersSignupAccess'));
     }
 
     @action
-    setDefaultEmailRecipients(value) {
-        this.segmentSelected = false;
-
-        if (['disabled', 'visibility'].includes(value)) {
-            this.settings.set('editorDefaultEmailRecipients', value);
+    setDefaultEmailRecipients(option) {
+        if (['disabled', 'visibility'].includes(option.value)) {
+            this.settings.set('editorDefaultEmailRecipients', option.value);
             return;
         }
 
-        if (value === 'none') {
+        if (option.value === 'none') {
             this.settings.set('editorDefaultEmailRecipientsFilter', null);
-        }
-
-        if (value === 'segment') {
-            this.segmentSelected = true;
         }
 
         this.settings.set('editorDefaultEmailRecipients', 'filter');
     }
 
     @action
-    setDefaultEmailRecipientsFilter(filter) {
-        this.settings.set('editorDefaultEmailRecipientsFilter', filter);
+    setSignupAccess(option) {
+        this.settings.set('membersSignupAccess', option.value);
+        this.args.onChange?.(option.value);
+
+        if (option.value === 'none') {
+            this.settings.set('defaultContentVisibility', 'public');
+        }
     }
 }
