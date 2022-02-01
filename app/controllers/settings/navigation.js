d8937796a963ddde56d681b5a07bed47838c45e6
Gabriel Csapo <gabecsapo@gmail.com>
2022-01-31 20:51:53 -0800

    [chore] rollback changes to app/controllers/settings/navigation.js

--- app/controllers/settings/navigation.js
+++ app/controllers/settings/navigation.js
@@ -1,151 +1,133 @@
-import classic from 'ember-classic-decorator';
-import {action, computed} from '@ember/object';
-import {inject as service} from '@ember/service';
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import $ from 'jquery';
 import Controller from '@ember/controller';
 import NavigationItem from 'ghost-admin/models/navigation-item';
 import RSVP from 'rsvp';
+import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
-@classic
-export default class NavigationController extends Controller {
-    @service
-    config;
+export default Controller.extend({
+    config: service(),
+    ghostPaths: service(),
+    notifications: service(),
+    session: service(),
+    settings: service(),
 
-    @service
-    ghostPaths;
-
-    @service
-    notifications;
-
-    @service
-    session;
-
-    @service
-    settings;
-
-    dirtyAttributes = false;
-    newNavItem = null;
-    newSecondaryNavItem = null;
-
+    dirtyAttributes: false,
+    newNavItem: null,
+    newSecondaryNavItem: null,
+    
     init() {
-        super.init(...arguments);
+        this._super(...arguments);
         this.set('newNavItem', NavigationItem.create({isNew: true}));
         this.set('newSecondaryNavItem', NavigationItem.create({isNew: true, isSecondary: true}));
-    }
+    },
 
-    @computed('config.blogUrl')
-    get blogUrl() {
+    blogUrl: computed('config.blogUrl', function () {
         let url = this.get('config.blogUrl');
 
         return url.slice(-1) !== '/' ? `${url}/` : url;
-    }
+    }),
 
-    @action
-    save() {
-        this.save.perform();
-    }
+    actions: {
+        save() {
+            this.save.perform();
+        },
 
-    @action
-    addNavItem(item) {
-        // If the url sent through is blank (user never edited the url)
-        if (item.get('url') === '') {
-            item.set('url', '/');
-        }
-
-        return item.validate().then(() => {
-            this.addNewNavItem(item);
-        });
-    }
-
-    @action
-    deleteNavItem(item) {
-        if (!item) {
-            return;
-        }
-
-        let navItems = item.isSecondary ? this.get('settings.secondaryNavigation') : this.get('settings.navigation');
-
-        navItems.removeObject(item);
-        this.set('dirtyAttributes', true);
-    }
-
-    @action
-    updateLabel(label, navItem) {
-        if (!navItem) {
-            return;
-        }
-
-        if (navItem.get('label') !== label) {
-            navItem.set('label', label);
-            this.set('dirtyAttributes', true);
-        }
-    }
-
-    @action
-    updateUrl(url, navItem) {
-        if (!navItem) {
-            return;
-        }
-
-        if (navItem.get('url') !== url) {
-            navItem.set('url', url);
-            this.set('dirtyAttributes', true);
-        }
-
-        return url;
-    }
-
-    @action
-    toggleLeaveSettingsModal(transition) {
-        let leaveTransition = this.leaveSettingsTransition;
-
-        if (!transition && this.showLeaveSettingsModal) {
-            this.set('leaveSettingsTransition', null);
-            this.set('showLeaveSettingsModal', false);
-            return;
-        }
-
-        if (!leaveTransition || transition.targetName === leaveTransition.targetName) {
-            this.set('leaveSettingsTransition', transition);
-
-            // if a save is running, wait for it to finish then transition
-            if (this.save.isRunning) {
-                return this.save.last.then(() => {
-                    transition.retry();
-                });
+        addNavItem(item) {
+            // If the url sent through is blank (user never edited the url)
+            if (item.get('url') === '') {
+                item.set('url', '/');
             }
 
-            // we genuinely have unsaved data, show the modal
-            this.set('showLeaveSettingsModal', true);
+            return item.validate().then(() => {
+                this.addNewNavItem(item);
+            });
+        },
+
+        deleteNavItem(item) {
+            if (!item) {
+                return;
+            }
+
+            let navItems = item.isSecondary ? this.get('settings.secondaryNavigation') : this.get('settings.navigation');
+
+            navItems.removeObject(item);
+            this.set('dirtyAttributes', true);
+        },
+
+        updateLabel(label, navItem) {
+            if (!navItem) {
+                return;
+            }
+
+            if (navItem.get('label') !== label) {
+                navItem.set('label', label);
+                this.set('dirtyAttributes', true);
+            }
+        },
+
+        updateUrl(url, navItem) {
+            if (!navItem) {
+                return;
+            }
+
+            if (navItem.get('url') !== url) {
+                navItem.set('url', url);
+                this.set('dirtyAttributes', true);
+            }
+
+            return url;
+        },
+
+        toggleLeaveSettingsModal(transition) {
+            let leaveTransition = this.leaveSettingsTransition;
+
+            if (!transition && this.showLeaveSettingsModal) {
+                this.set('leaveSettingsTransition', null);
+                this.set('showLeaveSettingsModal', false);
+                return;
+            }
+
+            if (!leaveTransition || transition.targetName === leaveTransition.targetName) {
+                this.set('leaveSettingsTransition', transition);
+
+                // if a save is running, wait for it to finish then transition
+                if (this.save.isRunning) {
+                    return this.save.last.then(() => {
+                        transition.retry();
+                    });
+                }
+
+                // we genuinely have unsaved data, show the modal
+                this.set('showLeaveSettingsModal', true);
+            }
+        },
+
+        leaveSettings() {
+            let transition = this.leaveSettingsTransition;
+            let settings = this.settings;
+
+            if (!transition) {
+                this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
+                return;
+            }
+
+            // roll back changes on settings props
+            settings.rollbackAttributes();
+            this.set('dirtyAttributes', false);
+
+            return transition.retry();
+        },
+
+        reset() {
+            this.set('newNavItem', NavigationItem.create({isNew: true}));
+            this.set('newSecondaryNavItem', NavigationItem.create({isNew: true, isSecondary: true}));
         }
-    }
+    },
 
-    @action
-    leaveSettings() {
-        let transition = this.leaveSettingsTransition;
-        let settings = this.settings;
-
-        if (!transition) {
-            this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
-            return;
-        }
-
-        // roll back changes on settings props
-        settings.rollbackAttributes();
-        this.set('dirtyAttributes', false);
-
-        return transition.retry();
-    }
-
-    @action
-    reset() {
-        this.set('newNavItem', NavigationItem.create({isNew: true}));
-        this.set('newSecondaryNavItem', NavigationItem.create({isNew: true, isSecondary: true}));
-    }
-
-    @task(function* () {
+    save: task(function* () {
         let navItems = this.get('settings.navigation');
         let secondaryNavItems = this.get('settings.secondaryNavigation');
 
@@ -178,8 +160,7 @@ export default class NavigationController extends Controller {
                 throw error;
             }
         }
-    })
-    save;
+    }),
 
     addNewNavItem(item) {
         let navItems = item.isSecondary ? this.get('settings.secondaryNavigation') : this.get('settings.navigation');
@@ -196,4 +177,4 @@ export default class NavigationController extends Controller {
             $('.gh-blognav-container:first .gh-blognav-line:last input:first').focus();
         }
     }
-}
+});
