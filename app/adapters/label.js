63012c1b4801698892fa816ac7dec38c3ce3884c
Gabriel Csapo <gabecsapo@gmail.com>
2022-02-01 21:34:58 -0800

    [chore] rollback migration

--- app/adapters/label.js
+++ app/adapters/label.js
@@ -1,6 +1,4 @@
 import ApplicationAdapter from 'ghost-admin/adapters/application';
 import SlugUrl from 'ghost-admin/mixins/slug-url';
-import classic from 'ember-classic-decorator';
 
-@classic
-export default class Label extends ApplicationAdapter.extend(SlugUrl) {}
+export default ApplicationAdapter.extend(SlugUrl);
