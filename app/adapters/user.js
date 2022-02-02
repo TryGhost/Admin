a244e1abbb5c51ef9d82f89288c68e87fc649ef8
Gabriel Csapo <gabecsapo@gmail.com>
2022-02-02 08:35:02 -0800

    [chore] migrate slug-url mixin to a utility

--- app/adapters/user.js
+++ app/adapters/user.js
@@ -1,9 +1,15 @@
 import ApplicationAdapter from 'ghost-admin/adapters/application';
-import SlugUrl from 'ghost-admin/mixins/slug-url';
+import SlugUrl from 'ghost-admin/utils/slug-url';
 import classic from 'ember-classic-decorator';
 
 @classic
-export default class User extends ApplicationAdapter.extend(SlugUrl) {
+export default class User extends ApplicationAdapter {
+    buildURL(_modelName, _id, _snapshot, _requestType, query) {
+        let url = super.buildURL(...arguments);
+
+        return SlugUrl(url, query);
+    }
+
     queryRecord(store, type, query) {
         if (!query || query.id !== 'me') {
             return super.queryRecord(...arguments);
