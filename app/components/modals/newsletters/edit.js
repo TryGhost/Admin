d352dd538326c093b7013b319b1710eca6a6a4e1
Peter Zimon <peter.zimon@gmail.com>
2022-04-28 10:02:38 +0200

    Swapped task for action

--- app/components/modals/newsletters/edit.js
+++ app/components/modals/newsletters/edit.js
@@ -27,7 +27,7 @@ export default class EditNewsletterModal extends Component {
         this.saveTask.perform();
     }
 
-    @task
+    @action
     toggleSection(section) {
         if (this.openSection === section) {
             this.openSection = null;
