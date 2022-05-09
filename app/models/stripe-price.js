73a1b9761b537e06675cd5ac84a6eb6ce91ee630
Hannah Wolfe <github.erisds@gmail.com>
2022-05-09 12:31:50 +0100

    revert Stripe Tier text

--- app/models/stripe-price.js
+++ app/models/stripe-price.js
@@ -3,7 +3,7 @@ import EmberObject from '@ember/object';
 export default EmberObject.extend({
     id: 'ID in Ghost',
     stripe_price_id: 'ID of the Stripe Price',
-    stripe_product_id: 'ID of the Stripe Tier the Stripe Price is associated with',
+    stripe_product_id: 'ID of the Stripe Product the Stripe Price is associated with',
     nickname: 'price nickname e.g. "Monthly"',
     description: 'price description e.g. "Full access"',
     amount: 'amount in smallest denomination e.g. cents, so value for 5 dollars would be 500',
